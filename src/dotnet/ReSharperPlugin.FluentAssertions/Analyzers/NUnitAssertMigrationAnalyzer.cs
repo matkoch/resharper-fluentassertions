b7dae5becaf8055e78c2ea2c074d1927ce980552
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2021-06-25 12:45:09 +0300

    Adds a class type check for Member Access Expression

--- src/dotnet/ReSharperPlugin.FluentAssertions/Analyzers/NUnitAssertMigrationAnalyzer.cs
+++ src/dotnet/ReSharperPlugin.FluentAssertions/Analyzers/NUnitAssertMigrationAnalyzer.cs
@@ -1,7 +1,11 @@
+using System.Linq;
+using JetBrains.Annotations;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
-using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.ReSharper.Psi.Resolve;
+using JetBrains.ReSharper.Psi.Resx.Utils;
 using NUnit.Framework;
 using ReSharperPlugin.FluentAssertions.Helpers;
 using ReSharperPlugin.FluentAssertions.Highlightings;
@@ -16,14 +20,14 @@ namespace ReSharperPlugin.FluentAssertions.Analyzers
         protected override void Run(IInvocationExpression element, ElementProblemAnalyzerData data,
             IHighlightingConsumer consumer)
         {
-            var references = element.GetPsiModule().GetPsiServices().Modules
-                .GetModuleReferences(element.GetPsiModule());
+            var psiModule = element.GetPsiModule();
+            var references = element.GetPsiServices().Modules.GetModuleReferences(psiModule);
             if (references.Count == 0)
             {
                 return;
             }
 
-            var testingFrameworkScanner = element.GetSolution().GetComponent<ITestingFrameworkScanner>();
+            var testingFrameworkScanner = psiModule.GetSolution().GetComponent<ITestingFrameworkScanner>();
 
             if (!testingFrameworkScanner.HasNUnit(references) ||
                 !testingFrameworkScanner.HasFluentAssertions(references))
@@ -31,12 +35,39 @@ namespace ReSharperPlugin.FluentAssertions.Analyzers
                 return;
             }
 
-            var invocationCodeString = element.InvokedExpression.GetText();
-            if (invocationCodeString == $"{nameof(Assert)}.{nameof(Assert.IsNotNull)}" ||
-                invocationCodeString == $"{nameof(Assert)}.{nameof(Assert.NotNull)}")
+
+            if (IsMemberAccessExpressionTypeOf<Assert>(element))
             {
                 consumer.AddHighlighting(new NUnitAssertMigrationHighlighting(element));
             }
+        }
+
+        private bool IsMemberAccessExpressionTypeOf<TType>([CanBeNull] IInvocationExpression invocationExpression)
+        {
+            if (invocationExpression?.Reference == null)
+            {
+                return false;
+            }
+
+            var expression = invocationExpression.InvokedExpression as IReferenceExpression;
+
+            if (!(expression?.ConditionalQualifier is IReferenceExpression qualifier))
+            {
+                return false;
+            }
+
+            var info = qualifier.Reference.Resolve();
+
+            return info.ResolveErrorType == ResolveErrorType.MULTIPLE_CANDIDATES
+                ? info.Result.Candidates.Any(IsTypeOf<TType>)
+                : IsTypeOf<TType>(info.DeclaredElement);
+        }
+
+        private bool IsTypeOf<TType>([CanBeNull] IDeclaredElement declaredElement)
+        {
+            var declaredElementAsString = declaredElement.ConvertToString();
+
+            return declaredElementAsString == $"Class:{typeof(TType).FullName}";
         }
     }
 }
