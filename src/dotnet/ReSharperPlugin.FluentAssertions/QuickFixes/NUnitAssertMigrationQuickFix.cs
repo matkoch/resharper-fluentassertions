7a0934ae8307aadda96c0f3ac21db8d6432c3c76
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2021-06-25 13:02:21 +0300

    Add XMLDOC for any classes

--- src/dotnet/ReSharperPlugin.FluentAssertions/QuickFixes/NUnitAssertMigrationQuickFix.cs
+++ src/dotnet/ReSharperPlugin.FluentAssertions/QuickFixes/NUnitAssertMigrationQuickFix.cs
@@ -12,6 +12,7 @@ using ReSharperPlugin.FluentAssertions.Highlightings;
 
 namespace ReSharperPlugin.FluentAssertions.QuickFixes
 {
+    /// <inheritdoc />
     [QuickFix]
     public class NUnitAssertMigrationQuickFix : QuickFixBase
     {
@@ -27,6 +28,7 @@ namespace ReSharperPlugin.FluentAssertions.QuickFixes
         {
             var invocationExpression = _highlighting.InvocationExpression;
             var factory = CSharpElementFactory.GetInstance(invocationExpression);
+            
             AddUsing(invocationExpression, factory);
 
             var expression = CreateNotBeNullExpression(factory);
@@ -43,12 +45,14 @@ namespace ReSharperPlugin.FluentAssertions.QuickFixes
         private ICSharpExpression CreateNotBeNullExpression(CSharpElementFactory factory)
         {
             var arguments = _highlighting.InvocationExpression.Arguments;
+            
             if (!arguments.Any())
             {
                 return null;
             }
 
             var expression = factory.CreateExpression("$0.Should().NotBeNull()", arguments.FirstOrDefault()?.GetText());
+            
             return expression;
         }
 
@@ -60,6 +64,7 @@ namespace ReSharperPlugin.FluentAssertions.QuickFixes
             }
 
             var fluentAssertionUsing = factory.CreateUsingDirective("FluentAssertions");
+            
             if (file.ImportsEnumerable.OfType<IUsingSymbolDirective>().All(i =>
                 i.ImportedSymbolName.QualifiedName != fluentAssertionUsing.ImportedSymbolName.QualifiedName))
             {
