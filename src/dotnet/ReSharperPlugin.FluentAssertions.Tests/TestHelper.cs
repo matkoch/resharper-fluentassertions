c02e57c4b42a4de41fdaa3d2a22a4db308d386d4
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2021-07-05 00:39:50 +0300

    Add TestCaseSource for dynamic test case detection for NUnitAssertNotNullHighlightingTests

--- src/dotnet/ReSharperPlugin.FluentAssertions.Tests/TestHelper.cs
+++ src/dotnet/ReSharperPlugin.FluentAssertions.Tests/TestHelper.cs
@@ -8,12 +8,24 @@ namespace ReSharperPlugin.FluentAssertions.Tests
     {
         public static TestCaseData[] FileNames(string folder)
         {
+            return FilteredFileNames(folder);
+        }
+
+        public static TestCaseData[] FilteredFileNames(string folder, string[] excludeItems = null)
+        {
             var strings = new[] {TestContext.CurrentContext.TestDirectory, @"..\..\..\test\data", folder}.ToArray();
             var testFileDirectory = Path.Combine(strings);
-
-            return Directory
+            var fileNames = Directory
                 .GetFiles(testFileDirectory, "*.cs")
-                .Select(x => new TestCaseData(Path.GetFileNameWithoutExtension(x)))
+                .Select(Path.GetFileNameWithoutExtension);
+
+            if (excludeItems != null && excludeItems.Any())
+            {
+                fileNames = fileNames.Where(x => !excludeItems.Contains(x));
+            }
+
+            return fileNames
+                .Select(x => new TestCaseData(x))
                 .ToArray();
         }
     }
