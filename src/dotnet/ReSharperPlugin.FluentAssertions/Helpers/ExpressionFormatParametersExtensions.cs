0d4c93f63e984671feb13bfd3a18c589036ed58f
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2021-11-19 23:56:13 +0300

    Fix InvariantCulture when formatting for GetExpressionFormatArguments

--- src/dotnet/ReSharperPlugin.FluentAssertions/Helpers/ExpressionFormatParametersExtensions.cs
+++ src/dotnet/ReSharperPlugin.FluentAssertions/Helpers/ExpressionFormatParametersExtensions.cs
@@ -8,7 +8,7 @@ namespace ReSharperPlugin.FluentAssertions.Helpers
     {
         public static string GetExpressionFormatArguments<TType>(this IEnumerable<TType> arguments)
         {
-            var groups = arguments.Skip(1).Select((item, index) => FormattableString.Invariant($"{index + 1}"));
+            var groups = arguments.Skip(1).Select((item, index) => FormattableString.Invariant($"${index + 1}"));
             return string.Join(", ", groups);
         }
     }
