ade8391632f285896c3e4c1bd9388cee8957a4a7
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2022-01-07 17:17:59 +0300

    get correct FluentAssertions `Should` method type by argument type

--- src/dotnet/ReSharperPlugin.FluentAssertions/Psi/FluentAssertionsPredefinedType.cs
+++ src/dotnet/ReSharperPlugin.FluentAssertions/Psi/FluentAssertionsPredefinedType.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 using JetBrains.Metadata.Reader.API;
 using JetBrains.Metadata.Reader.Impl;
@@ -14,19 +15,20 @@ namespace ReSharperPlugin.FluentAssertions.Psi
         public static readonly IClrTypeName ASSERT_FQN =
             new ClrTypeName(nameof(NUnit) + "." + nameof(NUnit.Framework) + "." + nameof(NUnit.Framework.Assert));
 
+        private static readonly IClrTypeName SHOULD_FQN =
+            new ClrTypeName(nameof(FluentAssertions) + ".AssertionExtensions");
+
         static FluentAssertionsPredefinedType()
         {
             // Collect predefined type names through reflection
             s_typeNameIndex = new Dictionary<IClrTypeName, int>();
 
             foreach (var info in typeof(FluentAssertionsPredefinedType).GetFields())
-            {
                 if (info.IsStatic && typeof(IClrTypeName).IsAssignableFrom(info.FieldType))
                 {
-                    var clrTypeName = (IClrTypeName) info.GetValue(obj: null);
+                    var clrTypeName = (IClrTypeName) info.GetValue(null);
                     s_typeNameIndex.Add(clrTypeName, s_typeNameIndex.Count);
                 }
-            }
         }
 
         [NotNull] private readonly IDeclaredType[] _types = new IDeclaredType[s_typeNameIndex.Count];
@@ -39,6 +41,18 @@ namespace ReSharperPlugin.FluentAssertions.Psi
         [NotNull] public IPsiModule Module { get; }
 
         [NotNull] public IDeclaredType Assert => CreateType(ASSERT_FQN);
+
+        [NotNull]
+        public IMethod GetShouldMethod(IType type)
+        {
+            var methods = TypeFactory
+                .CreateTypeByCLRName(SHOULD_FQN, Module)
+                .GetTypeElement().Methods
+                .Where(x => x.ShortName == "Should");
+
+            return methods
+                .FirstOrDefault(x => x.Parameters.Any(t => t.Type.Equals(type)));
+        }
 
         [CanBeNull]
         public IDeclaredType TryGetType([NotNull] IClrTypeName clrTypeName)
