011d3b3f37b554dbe7b244604079aa46d5ee41c6
mu88 <4560672+mu88@users.noreply.github.com>
2022-03-28 17:42:40 +0200

    Add tests for highlighting and quickfixes

---
+++ src/dotnet/ReSharperPlugin.FluentAssertions.Tests/test/data/NUnitAssertMigrationHighlightingTests/PositiveCaseTrue.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+public class ExampleTests
+{
+    [Test]
+    public void Test()
+    {
+        // arrange
+
+        // act
+        bool result = false;
+        
+        // assert
+        Assert.True(result);
+    }
+}
