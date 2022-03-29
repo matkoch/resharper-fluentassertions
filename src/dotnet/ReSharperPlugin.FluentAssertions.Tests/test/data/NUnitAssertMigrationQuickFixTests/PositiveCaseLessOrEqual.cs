25874e95cbab3d9a12d8f75cc9e2662e63fec9d1
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2022-03-29 14:05:29 +0300

    Add test cases for nUnit Assert.LessOrEqual

---
+++ src/dotnet/ReSharperPlugin.FluentAssertions.Tests/test/data/NUnitAssertMigrationQuickFixTests/PositiveCaseLessOrEqual.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+public class ExampleTests
+{
+    [Test]
+    public void Test()
+    {
+        // arrange
+
+        // act
+        int result = 10;
+        
+        // assert
+        {caret}Assert.LessOrEqual(19, result);
+    }
+}
