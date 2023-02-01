ba97d5b6785e8c720de29e90d03ef8c8588b234b
Matthias Koch <ithrowexceptions@gmail.com>
2023-02-01 10:54:24 +0100

    Fix usage of resources

---
+++ src/dotnet/ReSharperPlugin.FluentAssertions/Resources.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace ReSharperPlugin.FluentAssertions
+{
+  using System;
+  using JetBrains.Application.I18n;
+  using JetBrains.DataFlow;
+  using JetBrains.Diagnostics;
+  using JetBrains.Lifetimes;
+  using JetBrains.Util;
+  using JetBrains.Util.Logging;
+  
+  [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+  [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+  public static class Resources
+  {
+    private static readonly ILogger ourLog = Logger.GetLogger("ReSharperPlugin.FluentAssertions.Resources");
+
+    static Resources()
+    {
+      CultureContextComponent.Instance.WhenNotNull(Lifetime.Eternal, (lifetime, instance) =>
+      {
+        lifetime.Bracket(() =>
+          {
+            ourResourceManager = new Lazy<JetResourceManager>(
+              () =>
+              {
+                return instance
+                  .CreateResourceManager("ReSharperPlugin.FluentAssertions.Resources", typeof(Resources).Assembly);
+              });
+          },
+          () =>
+          {
+            ourResourceManager = null;
+          });
+      });
+    }
+    
+    private static Lazy<JetResourceManager> ourResourceManager = null;
+    
+    [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+    public static JetResourceManager ResourceManager
+    {
+      get
+      {
+        var resourceManager = ourResourceManager;
+        if (resourceManager == null)
+        {
+          return ErrorJetResourceManager.Instance;
+        }
+        return resourceManager.Value;
+      }
+    }
+
+    public static string LastResultMacroDefDescription => ResourceManager.GetString("LastResultMacroDefDescription");
+  }
+}
